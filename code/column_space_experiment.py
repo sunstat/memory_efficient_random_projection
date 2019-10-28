e87554317f621cf2e90866a7c7c99d8ea360dce0
nachtsky1077 <nachtsky1077@users.noreply.github.com>
2019-10-29 01:26:38 +0800

    update simulation process

--- code/column_space_experiment.py
+++ code/column_space_experiment.py
@@ -2,10 +2,11 @@ from tensorsketch.util import square_tensor_gen
 from .merp import Merp
 import numpy as np
 import tensorly as tl
+import pickle
 
 tl.set_backend('numpy')
 
-def exp_column_space(tensor, k, mode=1, method='normal', iteration=100):
+def run_exp(tensor, k, mode=1, method='normal', iteration=100):
     '''
     :tensor: generated tensor
     :k: reduced dimension
@@ -34,4 +35,27 @@ def exp_column_space(tensor, k, mode=1, method='normal', iteration=100):
         relative_err = err / np.linalg.norm(X, ord='fro')
         relative_errs.append(relative_err)
     return relative_errs
+
+
+def column_space_experiment(tensor_dim=[(900, 900), (400, 400, 400), (100, 100, 100, 100)], k=[5, 10, 15, 20, 25], mode=1, iteration=100):
+    # result dictionary
+    res = dict()
+    for dim in tensor_dim:
+        for reduced_k in k:
+            curr_res = dict()
+            # generate tensor
+            # 1. dense tensor
+            dense_tensor = tl.tensor(np.random.normal(loc=0.0, scale=1.0, size=dim))
+            # 2. square tensor
+            lr_tensor, _ = square_tensor_gen(n=dim[0], r=5, dim=len(dim))
+            for tensor_gen in ['dense_tensor', 'lr_tensor']:
+                for method in ['TRP', 'normal']:
+                    tensor = dense_tensor if tensor_gen == 'dense_tensor' else lr_tensor
+                    relative_err = run_exp(tensor=tensor, k=reduced_k, mode=mode, method=method, iteration=iteration)
+                    # store results
+                    curr_res[(tensor_gen, method)] = relative_err
+            res[(dim, reduced_k)] = curr_res
+    return res
+
+
 
