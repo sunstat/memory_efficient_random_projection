9f40265b2c67f21de6187be776c4055172851892
YanagiFive <kanaliushijun@gmail.com>
2019-10-30 17:16:59 +0800

    testDimension func-packed and test_shape bug

--- code/nearest_neighbor/recall_testDimension.py
+++ code/nearest_neighbor/recall_testDimension.py
@@ -3,28 +3,19 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestNeighbors as nn
 import logging
-from .merp import Merp
+from ..merp import Merp
 from .simulation import normalization, simulate
 import matplotlib.pyplot as plt
 import tensorly as tl
-if __name__ == '__main__':
-    print('hello')
-    mat = scipy.io.loadmat('./data/ImageNet_256.mat')
+
+
+def test_dimension(iter_steps, smallX):
     recallT = []
     recallF = []
     recallTU = []
     recallFU = []
     recallTL = []
     recallFL = []
-    iter_steps = 100
-    # print(mat.keys())
-    X = mat['X']
-    print(X.shape)
-    X = X[1:41, :]
-    smallX = tl.unfold(X, mode=0)
-    smallX = normalization(smallX)
-    print(smallX.shape)
-
     for k in range(10, 65, 5):
         rpb = Merp([16, 16], k, rand_type='g', target='col', tensor=False)
         X_train, X_test = train_test_split(
@@ -41,15 +32,21 @@ if __name__ == '__main__':
             rp_neigh = nn(n_neighbors=10)
             rp_neigh.fit(X_train_rp)
             # query and calculate recall rate
+            print(X_test.shape)
             true_neighbors = true_neigh.kneighbors(X_test)
             pred_neighbors = rp_neigh.kneighbors(X_test_rp)
+            # print('hello')
+            # print(len(true_neighbors))
+            # print(len(pred_neighbors))
+            # print(X_test.shape[0])
             curr_recall = np.asarray([np.intersect1d(
-                true_neighbors[i], pred_neighbors[i]).size for i in range(X_test.shape[0])]) / 10
+                true_neighbors[u], pred_neighbors[u]).size for u in range(X_test.shape[0])]) / 10
             recall = recall+curr_recall.mean()
             recalllist.append(curr_recall.mean())
         recallF.append(recall/iter_steps)
         recallFU.append(np.percentile(recalllist, 97.5))
         recallFL.append(np.percentile(recalllist, 2.5))
+    print('utada')
     print(recallF)
 
     for k in range(10, 65, 5):
@@ -70,14 +67,35 @@ if __name__ == '__main__':
             # query and calculate recall rate
             true_neighbors = true_neigh.kneighbors(X_test)
             pred_neighbors = rp_neigh.kneighbors(X_test_rp)
+            # print('hello')
+            # print(len(true_neighbors))
+            # print(len(pred_neighbors))
+            # print(len(X_test.shape))
             curr_recall = np.asarray([np.intersect1d(
-                true_neighbors[i], pred_neighbors[i]).size for i in range(X_test.shape[0])]) / 10
+                true_neighbors[u], pred_neighbors[u]).size for u in range(X_test.shape[0])]) / 10
             recall = recall+curr_recall.mean()
             recalllist.append(curr_recall.mean())
         recallT.append(recall/iter_steps)
         recallTU.append(np.percentile(recalllist, 97.5))
         recallTL.append(np.percentile(recalllist, 2.5))
+    print('shiina')
     print(recallT)
+    print(recallF)
+    return[recallF, recallFU, recallFL, recallT, recallTU, recallTL]
+
+
+if __name__ == '__main__':
+    mat = scipy.io.loadmat('./data/ImageNet_256.mat')
+    X = mat['X']
+    print(X.shape)
+    X = X[1:41, :]
+    smallX = tl.unfold(X, mode=0)
+    smallX = normalization(smallX)
+    print(smallX.shape)
+    iter_steps = 100
+
+    [recallF, recallFU, recallFL, recallT, recallTU,
+        recallTL] = test_dimension(iter_steps, smallX)
 
 
 x = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
--- code/nearest_neighbor/simulation.py
+++ code/nearest_neighbor/simulation.py
@@ -3,7 +3,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestNeighbors as nn
 import logging
-from .merp import Merp
+from ..merp import Merp
+
 
 def normalization(X, mode='l2'):
     if not mode in ['gaussian', 'l2']:
@@ -22,15 +23,17 @@ def normalization(X, mode='l2'):
         X = np.nan_to_num(X)
         return X
 
+
 def simulate(X, num_iters=10, n_neighbors=10, **merpCfg):
     # normalize input data
     X = normalization(X)
-    
+
     # the merp instance
     rp = Merp(merpCfg)
 
     # split train and test set
-    X_train, X_test = train_test_split(X, test_size=0.05, train_size=0.95, random_state=23)
+    X_train, X_test = train_test_split(
+        X, test_size=0.05, train_size=0.95, random_state=23)
 
     # generate ground truth
     true_neigh = nn(n_neighbors=10)
@@ -49,7 +52,8 @@ def simulate(X, num_iters=10, n_neighbors=10, **merpCfg):
         # query and calculate recall rate
         true_neighbors = true_neigh.kneighbors(X_test)
         pred_neighbors = rp_neigh.kneighbors(X_test_rp)
-        curr_recall = np.asarray([np.intersect1d(true_neighbors[i], pred_neighbors[i]).size for i in range(X_test.shape[0])]) / n_neighbors
+        curr_recall = np.asarray([np.intersect1d(
+            true_neighbors[i], pred_neighbors[i]).size for i in range(X_test.shape[0])]) / n_neighbors
         recall.append(curr_recall.mean())
     return recall
 
@@ -58,7 +62,3 @@ if __name__ == '__main__':
     mat = scipy.io.loadmat('./data/ImageNet_256.mat')
     print(mat.keys())
     X = mat['X']
-    
-    
-
-
