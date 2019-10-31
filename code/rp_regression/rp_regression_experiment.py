4307d40abe1153e5cd27d25620dc6cd0e7f25972
YanagiFive <kanaliushijun@gmail.com>
2019-10-31 23:37:34 +0800

    start simulate regression

--- code/rp_regression/rp_regression_experiment.py
+++ code/rp_regression/rp_regression_experiment.py
@@ -5,6 +5,7 @@ import numpy as np
 min ||Y - X\Omega\beta||_2
 '''
 
+
 def solve_linear_regression(X, y):
     '''
     return argmin_gamma (||Y - X\gamma||_2)^2
@@ -12,16 +13,18 @@ def solve_linear_regression(X, y):
     gamma, _, _, _ = np.linalg.lstsq(X, y)
     return gamma
 
-def design_matrix_gen(order, n, d, typ='g'):
+
+def design_matrix_gen(order, n, d, type='g'):
     Ai = []
-    if typ == 'g':
+    if type == 'g':
         for _ in range(order):
             Ai.append(np.random.normal(0.0, 1.0, size=(n, d)))
     else:
         raise NotImplementedError
     return Ai
 
-def beta_gen(d, typ='g'):
+
+def beta_gen(d, type='g'):
     if type == 'g':
         beta = np.random.normal(0.0, 1.0, size=(d,))
     elif type == 'u':
@@ -30,6 +33,7 @@ def beta_gen(d, typ='g'):
         raise NotImplementedError
     return beta
 
+
 def run_exp(X, y, tensor_dim, k, mode=0, method='TRP'):
     n, m = X.shape
     assert n == tensor_dim[mode]
@@ -37,28 +41,38 @@ def run_exp(X, y, tensor_dim, k, mode=0, method='TRP'):
     assert method in ['TRP', 'normal']
 
     del tensor_dim[mode]
-    rp = Merp(n=tensor_dim, k=k, rand_type='g', target='col', tensor=method=='TRP')
+    rp = Merp(n=tensor_dim, k=k, rand_type='g',
+              target='col', tensor=method == 'TRP')
     reduced_X = rp.transform(X)
 
     true_gamma = solve_linear_regression(X, y)
     esti_gamma = np.matmul(rp._omega, solve_linear_regression(reduced_X, y))
+    return [true_gamma, esti_gamma]
 
-    #TODO: do evaluation
-def evaluation(beta_hat,beta_true):
-    absError = []
+    # TODO: do evaluation
+
+
+def evaluation(beta_hat, beta_true):
     squaredError = []
     error = []
     for i in range(len(beta_true)):
         error.append(beta_true[i] - beta_hat[i])
     for val in error:
         squaredError.append(val * val)
-    #MSE
+    # MSE
     return sum(squaredError) / len(squaredError)
 
 
-    
-
-
-
-    
-
+if __name__ == '__main__':
+    X = np.array(design_matrix_gen(3, 2, 1, 'g'))
+    beta_o = np.array([beta_gen(2, 'g')])
+    y = np.matmul(X, beta_o)
+    print(X.shape)
+    print(beta_o.shape)
+    print(y.shape)
+    # for i in range(3):
+    #     X.append(design_matrix_gen(3, 2, 2, 'g'))
+    #     beta_o.append(beta_gen(2, 'g'))
+    #     y.append(np.matmul(X[i], beta_o[i]))
+    [true_gamma, esti_gamma] = run_exp(X, y, [2, 1], 2, 0, 'TRP')
+    # gamma_MSE = evaluation(estigamma, true_gamma)
