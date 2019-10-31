9d9ebf44694425a9a4933929bd06906e72421f76
nachtsky1077 <44351210+nachtsky1077@users.noreply.github.com>
2019-10-31 14:19:40 +0800

    add fast qr for merp

--- code/merp.py
+++ code/merp.py
@@ -1,12 +1,47 @@
 from tensorsketch.random_projection import random_matrix_generator as rm_gen
 from tensorsketch.random_projection import tensor_random_matrix_generator as trm_gen
 import numpy as np
-        
+import tensorly as tl
+
 np.random.seed(233)
+
+# tensor sketch method returning the small matrices before k
+def tensor_random_matrix_generator_sp(n_arr, k, typ="g", target='col'):
+    """
+    routine for usage: A \Omega or \Omega^\top x : n >> m
+    :param n_arr: first dimension of random matrix to be generated as a list
+    n1*...n_{I+1}*...*n_N
+    :param k: second dimension of random matrix to be generated
+    :param type:
+    :param target:  for column preservation or length preservation,
+    for column preservation, we do not need standardization
+    :return:
+    """
+    if not isinstance(n_arr, list):
+        raise Exception("type of first parameter must be list")
+
+    types = set(['g', 'u', 'sp0', 'sp1'])
+    assert typ in types, "please aset your type of random variable correctly"
+    assert target in ['col', 'vec'], "target can only be col or vec"
+    Omegas = []
+    for n in n_arr:
+        if typ == 'g':
+            Omega = np.random.normal(0, 1, size=(n, k))
+        elif typ == 'u':
+            Omega = np.random.uniform(low=-1, high=1, size=(n, k)) * np.sqrt(3)
+        elif typ == 'sp0':
+            Omega = np.random.choice([-1, 0, 1], size=(n, k), p=[1 / 6, 2 / 3, 1 / 6]) * np.sqrt(3)
+        elif typ == 'sp1':
+            Omega = np.random.choice([-1, 0, 1], size=(n, k), p= \
+                [1 / (2 * np.sqrt(n)), 1 - 1 / np.sqrt(n), 1 / (2 * np.sqrt(n))]) * np.sqrt(np.sqrt(n))
+        Omegas.append(Omega)
+    if target == 'col':
+        return Omegas, tl.tenalg.khatri_rao(Omegas)
+    return Omegas, tl.tenalg.khatri_rao(Omegas).transpose()/np.sqrt(k)
 
 class Merp(object):
 
-    def __init__(self, n, k, rand_type='g', target='col', tensor=False):
+    def __init__(self, n, k, rand_type='g', target='col', tensor=False, fastQR=False):
         '''
         :param: n, first dimension of random matrix to be generated
         :param: k, second dimension of random matrix to be generated
@@ -18,18 +53,44 @@ class Merp(object):
         self._type = rand_type
         self._target = target
         self._rm_gen = trm_gen if tensor else rm_gen
-        
+        self._tensor = tensor
+        self._fastQR = fastQR
+        if fastQR and tensor:
+            self._rm_gen_sp = tensor_random_matrix_generator_sp
+
         # generate omega
-        self._omega = self._rm_gen(n, k=k, typ=rand_type, target=target)
+        if fastQR and tensor:
+            self._omegas, self._omega = self._rm_gen_sp(n, k=k, typ=rand_type, target=target)
+        else:
+            self._omega = self._rm_gen(n, k=k, typ=rand_type, target=target)
 
     def regenerate_omega(self):
-        self._omega = self._rm_gen(self._n, 
-                                   k=self._k, 
-                                   typ=self._type, 
-                                   target=self._target)
+        if self._fastQR and self._tensor:
+            self._omegas, self._omega = self._rm_gen_sp(self._n, k=self._k, typ=self._type, target=self._target)
+        else:
+            self._omega = self._rm_gen(self._n, k=self._k, typ=self._type, target=self._target)
     
+    def fastQR(self, X):
+        '''
+        :X: a 3-D tensor
+        '''
+        assert self._fastQR and self._tensor
+        assert X.shape[0] == len(self._omegas)
+        # qr decompose of Xi\Omega_i
+        qs = []
+        rs = []
+        for i in range(X.shape[0]):
+            q, r = np.linalg.qr(np.matmul(X[i], self._omegas[i]))
+            qs.append(q)
+            rs.append(r)
+        
+        q = tl.tenalg.kronecker(qs)
+        r = tl.tenalg.khatri_rao(rs)
+        return q, r
+
     def transform(self, X):
         n, d = X.shape
         _n = np.prod(self._n) if isinstance(n, list) else self._n
         assert d == np.prod(_n)
         return np.matmul(X, self._omega)
+
--- code/rp_regression/rp_regression_experiment.py
+++ code/rp_regression/rp_regression_experiment.py
@@ -1,12 +1,34 @@
 from ..merp import Merp
 import numpy as np
 
+'''
+min ||Y - X\Omega\beta||_2
+'''
+
 def solve_linear_regression(X, y):
     '''
     return argmin_gamma (||Y - X\gamma||_2)^2
     '''
     gamma, _, _, _ = np.linalg.lstsq(X, y)
     return gamma
+
+def design_matrix_gen(order, n, d, typ='g'):
+    Ai = []
+    if typ == 'g':
+        for _ in range(order):
+            Ai.append(np.random.normal(0.0, 1.0, size=(n, d)))
+    else:
+        raise NotImplementedError
+    return Ai
+
+def beta_gen(d, typ='g'):
+    if type == 'g':
+        beta = np.random.normal(0.0, 1.0, size=(d,))
+    elif type == 'u':
+        beta = np.random.uniform(-1.0, 1.0, size=(d,))
+    else:
+        raise NotImplementedError
+    return beta
 
 def run_exp(X, y, tensor_dim, k, mode=0, method='TRP'):
     n, m = X.shape
